cb5bcd13164f1e087b32f9bf7e8f88fd2531be62
DenisMuydinov <161165375+DenisMuydinov@users.noreply.github.com>
2024-02-25 02:30:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ flag = "unclicked";
 function doathing() {
   if (flag == "unclicked") {
     document.getElementById("demo").innerHTML = "I like when your mom is cuming( ͡° ͜ʖ ͡°)";
+    alert('( ͡° ͜ʖ ͡°)');
     return flag = "clicked";
   } else {
     document.getElementById("demo").innerHTML = "and screaming my name in my bed( ͡° ͜ʖ ͡°)";
+    alert('( ͡° ͜ʖ ͡°)');
     return flag = "unclicked";
   }
 }
