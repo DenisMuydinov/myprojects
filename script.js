e56c70212282446773c4648a75edde5273b82b6a
DenisMuydinov <161165375+DenisMuydinov@users.noreply.github.com>
2024-02-25 01:52:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+flag = "unclicked";
+function doathing() {
+  if (flag == "unclicked") {
+    document.getElementById("demo").innerHTML = "I like when your mom is cuming( ͡° ͜ʖ ͡°)";
+    return flag = "clicked";
+  } else {
+    document.getElementById("demo").innerHTML = "and screaming my name in my bed( ͡° ͜ʖ ͡°)";
+    return flag = "unclicked";
+  }
+}
